2550ba326ca3862e89294ef778328a4dcde864cc
Dave Winer <dave.winer@gmail.com>
2015-11-27 12:34:53 -0500

    v0.40

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-The client opens a websocket with the server, and the server posts a message to the socket every second.
+#### betterWebSocketsDemo
+
+The client opens a socket with the server, and the server posts a message to the socket every second.
+
+#### How to test
+
+1. In your terminal app, cd to the server folder.
+
+2. Enter <code>npm install</code>. 
+
+3. Enter <code>node server.js</code> to launch the server app.
+
+4. Open the client app in a WebSockets-compatible browser.
+
+5. Open the JavaScript console so you can see what's coming into the browser app.
+
+6. Close the browser tab and watch what happens in the server. (It notices the connection has closed and stops sending messages.)
+
+7. You can open two or more instances of client.html, they should all work.
+
+#### Code review please
+
+If you have experience working in WebSockets and you think there's a problem, please post a comment here. 
+
+I'm not only posting this to help others, but also to be sure I'm using WebSockets correctly and in the most efficient way.
 
--- server/server.js
+++ server/server.js
@@ -7,22 +7,26 @@ var ctMessages = 0;
 function everySecond () {
 	if (myConnection !== undefined) {
 		var theMsg = "Hello there #" + ctMessages++;
+		console.log ("everySecond: sending \"" + theMsg + "\"");
 		myConnection.sendText (theMsg);
 		}
 	}
 function handleConnection (conn) { 
 	conn.on ("text", function (s) {
 		myConnection = conn;
-		console.log ("The connection is open. Get ready for some interesting messages!");
+		console.log ("'text' message received. Sending a message to the socket every second.");
 		});
 	conn.on ("close", function () {
-		myConnection = undefined;
-		console.log ("The connection is closed.");
+		console.log ("'close' message received.");
+		if (myConnection !== undefined) {
+			console.log ("Closing the connection.");
+			myConnection = undefined;
+			}
 		});
 	conn.on ("error", function () {
-		myConnection = undefined;
-		console.log ("There was an error. The connection is closed.");
+		console.log ("'error' message received.");
 		});
 	}
+console.log ("\n" + myProductName + " v" + myVersion + "\n");
 ws.createServer (handleConnection).listen (myPort);
 setInterval (everySecond, 1000); 
