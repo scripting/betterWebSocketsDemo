fdffda9138eb1d6ef8b0a70e8061069328f39f05
Dave Winer <dave.winer@gmail.com>
2015-11-27 12:18:16 -0500

    v0.40

---
+++ README.md
@@ -0,0 +1,2 @@
+The clie nt opens a websocket with the server, and the server posts a message to the socket every second.
+
---
+++ server/server.js
@@ -0,0 +1,27 @@
+var ws = require ("nodejs-websocket");
+var myPort = 1337; 
+var myConnection;
+var ctMessages = 0;
+
+function everySecond () {
+	if (myConnection !== undefined) {
+		var theMsg = "Hello there #" + ctMessages++;
+		myConnection.sendText (theMsg);
+		}
+	}
+function handleConnection (conn) { 
+	conn.on ("text", function (s) {
+		myConnection = conn;
+		console.log ("The connection is open. Get ready for some interesting messages!");
+		});
+	conn.on ("close", function () {
+		myConnection = undefined;
+		console.log ("The connection is closed.");
+		});
+	conn.on ("error", function () {
+		myConnection = undefined;
+		console.log ("There was an error. The connection is closed.");
+		});
+	}
+ws.createServer (handleConnection).listen (myPort);
+setInterval (everySecond, 1000); 
